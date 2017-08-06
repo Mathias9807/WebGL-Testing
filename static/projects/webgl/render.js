25e4947fbfc8c1a37258844c6be6e4e3ca6fd452
Mathias Johansson <mathias9807@gmail.com>
2017-08-06 10:23:10 +0200

    Added controls

--- static/projects/webgl/render.js
+++ static/projects/webgl/render.js
@@ -20,14 +20,77 @@ class Render {
 		gl.useProgram(shader);
 		gl.bindAttribLocation(shader, 0, "vertex_in");
 		gl.bindAttribLocation(shader, 1, "uv_in");
+
+		this.shader = shader;
 	}
 
-	render() {
+	setParam3f(uniform, vec) {
+		var loc = gl.getUniformLocation(this.shader, uniform);
+		gl.uniform3fv(loc, vec);
+	}
+
+	setParam4x4f(uniform, mat) {
+		var loc = gl.getUniformLocation(this.shader, uniform);
+		gl.uniformMatrix4fv(loc, false, mat);
+	}
+
+	render(camera) {
 		// Clear screen
-		gl.clearColor(1, 1, 1, 0);
+		gl.clearColor(1, 1, 1, 1);
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+		this.setParam4x4f("mat_proj", camera.getProjMatrix());
+		this.setParam4x4f("mat_world", camera.getWorldMatrix());
+
+		var modelMat = mat4.create();
+		mat4.fromScaling(modelMat, vec3.fromValues(0.5, 0.5, 0.5));
+		this.setParam4x4f("mat_model", modelMat);
+
 		model.render();
+	}
+
+}
+
+class Camera {
+
+	constructor() {
+		this.pos = vec3.create();
+		this.rot = vec3.create();
+
+		this.fov = 90 * 3.1415926535 / 180;
+	}
+
+	moveForward(d) {
+		this.pos[2] += d * -Math.cos(this.rot[1]);
+		this.pos[0] += d * -Math.sin(this.rot[1]);
+	}
+
+	moveRight(d) {
+		this.pos[0] += d * Math.cos(this.rot[1]);
+		this.pos[2] += d * -Math.sin(this.rot[1]);
+	}
+
+	getWorldMatrix() {
+		var world = mat4.create();
+
+		mat4.rotateZ(world, world, -this.rot[2]);
+		mat4.rotateX(world, world, -this.rot[0]);
+		mat4.rotateY(world, world, -this.rot[1]);
+
+		var trans = vec3.create();
+		vec3.negate(trans, this.pos);
+		mat4.translate(world, world, trans);
+
+		return world;
+	}
+
+	getProjMatrix() {
+		var proj = mat4.create();
+
+		var aspect = windowNode.width / windowNode.height;
+		mat4.perspective(proj, this.fov / aspect, aspect, 0.001, 100);
+
+		return proj;
 	}
 
 }
--- static/projects/webgl/script.js
+++ static/projects/webgl/script.js
@@ -4,20 +4,48 @@ windowNode.width = window.devicePixelRatio
 windowNode.height = window.devicePixelRatio
 	* windowNode.offsetHeight;
 
+var keys = [];
+keys["w"] = keys["a"] = keys["s"] = keys["d"] = keys[" "] = keys["shift"] = false;
+keys["arrowright"] = keys["arrowleft"] = false;
+
 var gl = windowNode.getContext('webgl2');
 
 var render = new Render();
 
+var camera = new Camera();
+camera.pos[1] = 0.5;
+camera.pos[2] = 5;
+
+// camera.rot[0] = -0.5;
+// camera.rot[1] = 0.1;
+
 var model = new Model("Simple bake.obj", null);
 
 function loop() {
+	var dTime = 0.01;
+
+	camera.moveForward((keys["w"] - keys["s"]) * 10 * dTime);
+	camera.moveRight((keys["d"] - keys["a"]) * 10 * dTime);
+
+	camera.rot[1] -= (keys["arrowright"] - keys["arrowleft"]) * 3 * dTime;
+
 	try {
-		render.render();
+		render.render(camera);
 	}catch(err) {
 		console.log(err);
 	}
 
-	setTimeout(loop, 200);
+	setTimeout(loop, dTime * 1000);
 }
 loop();
+
+function keyPressed(e) {
+	keys[e.key.toLowerCase()] = true;
+}
+document.addEventListener("keydown", keyPressed);
+
+function keyReleased(e) {
+	keys[e.key.toLowerCase()] = false;
+}
+document.addEventListener("keyup", keyReleased);
 
